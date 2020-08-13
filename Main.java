90c46bdcce9418a026c74e5c5fbb83f43b8dc762
wilsonlaww <69646455+wilsonlaww@users.noreply.github.com>
2020-08-13 11:02:51 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	} System.out.println("My name is Wilson Law");
+	 System.out.println("I love the outdoors");
+	  System.out.println("I play waterpolo and swim");
+	   System.out.println("My dream college is UCLA");
+}
